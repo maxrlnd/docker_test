21b72608aa8df453fd244e496693ff969776c03c
tshrum <trisha.shrum@colorado.edu>
2016-07-26 14:53:46 -0600

    First commit

---
+++ master.R
@@ -0,0 +1,20 @@
+# Copyright (c) 2016 Trisha Shrum, Joseph Tuccillo
+#
+# Authors Comment: This model is jointly developed at the University of 
+#  Colorado Earth Lab based on work by Adam ??, Joseph Tuccillo, Kelly Carney, 
+#  Bill Travis, Jeffrey Tranel, Rod Sharp, and John Deering.
+#
+# Description: This script implements a simulation of drought adaptation
+#  decisions by Western cattle ranchers. 
+#
+# Inputs:
+#   ...
+#
+# Outputs:
+#   ...
+
+# Ancillary source scripts
+
+# Library functions
+
+# Main Script
