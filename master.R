96bc005d54b707e81c4068baf05603dd37f6fce2
William Travis <William Travis>
2016-08-02 16:32:18 -0600

    Vectorized input variables that are dynamic by year in vars.R; in master.R computed outcome matrices (costs and revenue) for each five-year scenario.

--- master.R
+++ master.R
@@ -15,46 +15,84 @@
 # Clear environment
 rm(list=ls())
 
-# Source variable assignment script
-source("R/vars.R")
-
 # Source functions
 source("R/support_functions.R")
+
+# Source variable assignment script
+source("R/vars.R")
 
 
 #### Main Script ####
 
-# Calculate No-Drought Revenues
-exp.sales <- CalculateExpSales(herd = herd, calf.sell = calf.sell, wn.wt = wn.wt, p.wn.yr1 = p.wn.yr1)
+# Calculate No-Drought Revenues (expected sale price, year 1)
+base.sales <- unlist(lapply(1:5,function(i){
+  CalculateExpSales(herd = herd, calf.sell = calf.sell, wn.wt = wn.wt, p.wn.yr1 = p.wn[i])
+}))
+
+base.op.cost = CalculateBaseOpCosts(herd = herd, cow.cost = cow.cost)
 
 # Compute insurance premiums and indemnities
-if (!is.null(insp)){
+if (purchase.insurance==1){
   rma.ins = insMat(tgrd = tgrd,yyr = yyr,clv = clv,acres = acres,
                    pfactor = pfactor,insPurchase  =  insp)
-}else{ # if insp is set as NULL (no insurance), simply set prem/indem = 0
+}else{ # if purchase.insurance set to 0 (no insurance), simply set prem/indem = 0
   rma.ins = cbind(yyr[1]:yyr[1]+4,matrix(0,5,2))
 }
 
-i = 1 # Current year - assuming year 1 for now, should be iterated
+base.cost = base.op.cost + rma.ins[,2] # increment base operating costs with producer premium
+base.rev = base.sales + rma.ins[,3] # increment base revenue with indemnity
+
+####No Drought####
+
+baseOutcome=cbind(base.sales,base.op.cost)
+
+####Drought Occurs####
+# For each option, we calculate the **CHANGE** in costs
+# and the **CHANGE** in revenues relative to the no drought baseline.
+
+## No adaptive action
+#this computation should be wrapped into a function!!
+forage.weights=unlist(lapply(seq(yyr,yyr+4),function(i){
+  foragePWt(stgg,zonewt,stzone,i)
+}))
+calf_weights_ann=unlist(lapply(forage.weights,function(i){ # annual calf weights
+  calfDroughtWeight(calf_wean,calf_currently,i)
+}))
+noAdaptOutcome=cbind(herd*calf.sell*calf_weights_ann*p.wn,
+                 base.cost)
 
 # Calculate days of drought adaptation action
 days.act <- CalculateDaysAction(act.st.yr,act.st.m,act.end.yr,act.end.m)
 
-# For each option, we calculate the **CHANGE** in costs and the **CHANGE** in revenues relative to the no drought baseline.
-# Option 1: Buy additional feed
+## Option 1: Buy additional feed
 days.feed <- days.act  # Assumes that feeding days are equivalent to drought adaptation action days
-feed.cost <- CalculateFeedCost(kHayLbs, kOthLbs, p.hay, p.oth, days.feed, herd) + rma.ins[,2][i] # Calculates additional costs to feed herd
 
-# Option 2: Truck pairs to rented pasture
+# Calculate additional costs to feed herd
+feed.cost <- unlist(lapply(1:5,function(i){
+  CalculateFeedCost(kHayLbs, kOthLbs, p.hay, p.oth, days.feed, herd) + base.cost[i]
+  })) 
+feedOutcome=cbind(base.rev,feed.cost+base.cost) # generate outcome matrix: col1 - revenue, col2 - cost
+
+## Option 2: Truck pairs to rented pasture
 days.rent <- days.act # Assumes that pasture rental days are equivalent to drought adaptation action days
-cost.rentpast <- CalculateRentPastCost(n.miles = n.miles, truck.cost = truck.cost, past.rent = past.rent,
-                                       days.rent = days.rent, oth.cost = oth.cost, max.wt = max.wt,
-                                       cow.wt = cow.wt, calf.wt = calf.wt, herd = herd) + rma.ins[,2][i]
-rev.rentpast <- CalculateRentPastRevenue(wn.wt = wn.wt, calf.loss = calf.loss, calf.wt.adj = calf.wt.adj, 
-                                         calf.sell = calf.sell, herd = herd, p.wn.yr1 = p.wn.yr1) + rma.ins[,3][i]
+rentpast = lapply(1:5,function(i){
+  c(CalculateRentPastRevenue(wn.wt = wn.wt, calf.loss = calf.loss[i], calf.wt.adj = calf.wt.adj[i],
+                             calf.sell = calf.sell, herd = herd, p.wn.yr1 = p.wn[i]) + base.rev[i],
+        CalculateRentPastCost(n.miles = n.miles, truck.cost = truck.cost, past.rent = past.rent,
+                          days.rent = days.rent, oth.cost = oth.cost, max.wt = max.wt,
+                          cow.wt = cow.wt, calf.wt = calf.wt, herd = herd) + base.cost[i])
+  })
+rentOutcome = t(matrix(unlist(rentpast),2,5)) # convert list to matrix
 
-# Option 3: Sell pairs and replace cows
-cost.sellprs.yr1 <- CalculateSellPrsCostYr1(op.cost.yr1 = op.cost.yr1, herd = herd, sell.cost = sell.cost) + rma.ins[,2][i]
-rev.sellprs.yr1 <- CalculateSellPrsRevenueYr1(herd = herd, calf.sell = calf.sell, wn.wt = wn.wt, 
-                                              p.wn.yr1 = p.wn.yr1, wn.succ = wn.succ, calf.wt = calf.wt, 
-                                              p.calf.t0 = p.calf.t0, p.cow = p.cow) + rma.ins[,3][i]
+
+## Option 3: Sell pairs and replace cows
+# cost.sellprs.yr1 <- CalculateSellPrsCostYr1(op.cost.yr1 = op.cost.yr1, herd = herd, sell.cost = sell.cost) + rma.ins[,2][i]
+# rev.sellprs.yr1 <- CalculateSellPrsRevenueYr1(herd = herd, calf.sell = calf.sell, wn.wt = wn.wt, 
+#                                               p.wn.yr1 = p.wn[i], wn.succ = wn.succ, calf.wt = calf.wt, 
+#                                               p.calf.t0 = p.calf.t0, p.cow = p.cow, exp.sales = exp.sales) + rma.ins[,3][i]
+lapply(1:5,function(i){
+  c(CalculateSellPrsRevenueYr1(herd = herd, calf.sell = calf.sell, wn.wt = wn.wt, 
+                               p.wn.yr1 = p.wn[i], wn.succ = wn.succ, calf.wt = calf.wt, 
+                               p.calf.t0 = p.calf.t0, p.cow = p.cow, exp.sales = exp.sales,invst.int = invst.int) + base.rev,
+    CalculateSellPrsCostYr1(op.cost.yr1 = op.cost.yr1, herd = herd, sell.cost = sell.cost) + base.cost)
+})
