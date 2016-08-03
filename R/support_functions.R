c8f80de8cfbb55764bc15afa5a9f9d7f12790f92
tshrum <trisha.shrum@colorado.edu>
2016-08-03 17:46:39 -0600

    Added function that calculates major accounting quantities (profit, taxes, etc) for each option (no drought, no adapt, feed, etc) and stores those values in a data.frame. Dataframe should be ready to append together different options and includes values with and without insurance. Still needs to be tested for options 1-4 (feed, rentpast, sellrepl, sellnorepl)

--- R/support_functions.R
+++ R/support_functions.R
@@ -644,3 +644,58 @@ calfWeanWeight<-function(styr){
   return(calf_weights_ann)
   
 }
+
+OptionOutput <- function(opt, nodrought = FALSE, rev.calf, rev.oth = NULL, cost.op, rma.ins, int.invst, int.loan) {
+  # Function: OptOutput
+  # Desciption: Takes in cost and revenue variables and outputs data.frame with 
+  # all relevant outcome variables
+  #
+  # Inputs:
+  # opt = String to label option: "nodrght", "noadpt", etc.
+  # nodrought = OPTIONAL value. default is set to false. set to true for no drought option
+  # rev.calf = 5x1 vector of revenue from actual calf sales in years 1 through 5
+  # rev.oth = OPTIONAL 5x1 vector of non-calf revenues (created to account for interest on sale of cows in year 1)
+  # int.invst = Interest rate on investments
+  # int.loan = Interest rate on loans
+  # rma.ins = 5x3 matrix of insurance year, premium, and payouts
+  # cost.op = 5x1 vector of operating costs for years 1 through 5, including any adaptation costs
+  
+  opt <- rep(opt, 11)
+  t <- c(0,rep(1:5,2))
+  ins <- c(0,rep(1,5),rep(0,5))
+  
+  rev.calf <- c(0,rep(rev.calf, 2))
+  cost.op <- c(0, rep(cost.op, 2))
+  
+  if(nodrought == FALSE) {
+    rev.ins <- c(0, rma.ins[, 3], rep(0, 5))  # potential payout for 2:6, no payout for 7:11
+  } else {
+    rev.ins <- rep(0,11) # no drought, no payout
+  }
+  
+  cost.ins <- c(0, rma.ins[, 2], rep(0,5))  # insurance for 2:6, no insurance for 7:11
+  
+  rev.int <- c(0, rep(0, 10)) #PLACEHOLDER. NOT CURRENTLY CALCULATING INTEREST
+  cost.int <- c(0, rep(0, 10))  # PLACEHOULDER. NOT CURRENTLY CALCULATING INTEREST
+  
+  if(!is.null(rev.oth)) {  # if other revenues are passed through, then they are included in total revenues
+    rev.oth <- c(0, rep(rev.oth, 2))
+    rev.tot <- rev.calf + rev.ins + rev.int + rev.oth
+  } else {
+    rev.tot <- rev.calf + rev.ins + rev.int
+  }
+  
+  cost.tot <- cost.op + cost.ins + cost.int
+  profit <- rev.tot - cost.tot
+  taxes <- ifelse(profit > 0, profit * (0.124+0.15+0.04), 0)  # taxes only if positive profits. i wonder if EITC applies here?
+  aftax.inc <- profit - taxes
+  cap.sales <- rep(NA, 11)
+  cap.taxes <- rep(NA, 11)
+  assets.cow <- rep(NA, 11)
+  assets.cash <- rep(NA, 11)
+  net.wrth <- rep(NA, 11)
+  
+  out <- cbind(opt, t, ins, rev.calf, rev.ins, rev.int, rev.tot, cost.op, 
+               cost.ins, cost.int, cost.tot, profit, taxes, aftax.inc,
+               cap.sales, cap.taxes, assets.cow, assets.cash, net.wrth)
+}  
