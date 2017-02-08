0a9b97e37f058675c8c35ea924a46ea5e826711b
tshrum <trisha.shrum@colorado.edu>
2017-02-08 12:15:41 -0700

    replaced `drght.act.adj' with `intens.adj'

--- R/support_functions.R
+++ R/support_functions.R
@@ -969,13 +969,13 @@ CalculateAdaptationIntensity <- function(forage.potential, drought.adaptation.co
         replacement.)
       forage.potential (the percentage of average forage produced in a year 
         based on rainfall. See forage potential functions.)
-    Output: drght.act.adj (scales action to account for forage potential's 
+    Output: intens.adj (scales action to account for forage potential's 
       deviation from the norm.)
     Assumptions: The variable has a maximum of 1, which assumes that drought 
       actions are parameterized at full forage replacement for the full herd.
   "
   intens.adj <- ifelse(forage.potential >= 1, 0, (1 - forage.potential) * drought.adaptation.cost.factor)
-  intens.adj <- ifelse(drght.act.adj > 1, 1, drght.act.adj)  # putting a ceiling of this variable at 1 (no more than 100% of drought action)
+  intens.adj <- ifelse(intens.adj > 1, 1, intens.adj)  # putting a ceiling of this variable at 1 (no more than 100% of drought action)
   intens.adj
 }
 
