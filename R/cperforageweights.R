ae8ff86a6924400af575b01b997c63115b4c583f
Trisha Shrum <trishashrum@gmail.com>
2017-03-10 15:21:54 -0700

    Added new forage weight calculations from MLRA data

---
+++ R/cperforageweights.R
@@ -0,0 +1,4 @@
+cpermlra <- c(0.01, 0.01, 0.05, 0.08,0.17,0.21,0.17,0.13,0.11,0.05,0.01,0.01)
+months <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+cper <- data.frame(cpermlra, months)
+
--- master.R
+++ master.R
@@ -45,7 +45,7 @@ generateRunParams <- function(acres.param = 3000){
 
 
 #### Non-parallel model run ####
-runs <- 1
+runs <- 100
 simruns <- rlply(runs, generateRunParams(acres.param = 3000))  # list of simulation variables for runs
 list.index <- seq_along(simruns)  # creating an index of the list number to store in the sim_outcomes and match back with the simruns variables
 for (i in 1:runs) {
