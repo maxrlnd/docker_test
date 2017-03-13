b6e5a7f30b2bf4bb90dd198f605a51b53663f36b
Adam McCurdy <admccurdy@gmail.com>
2017-03-13 08:07:28 -0600

    removed global variables, created year1 UI

--- ui.R
+++ ui.R
@@ -41,44 +41,7 @@ navbarPageWithJS <- function(..., text) {
   navbar
 }
 
-#### Setup ####
 
-# Populate a new environment with station gauge info.
-# Default location is CPER site
-station.gauge <- getStationGauge()
-
-# Populate a new environment with constant (user) variables
-constvars <- getConstantVars()
-
-practiceVars <- getSimVars(
-  station.gauge,
-  constvars,
-  start_year = 2002,
-  sim_length = 3,
-  use.forage = T,
-  random.acres=FALSE,
-  random.productivity=TRUE,
-  acres = 3000)
-
-simvars <- getSimVars(
-  station.gauge,
-  constvars,
-  start_year = 2002,
-  sim_length = 10,
-  use.forage = T,
-  random.acres=FALSE,
-  random.productivity=TRUE,
-  acres = 3000)
-
-practiceRuns <- (append(append(station.gauge, constvars), (practiceVars)))
-simRuns <- (append(append(station.gauge, constvars), (simvars)))
-
-practiceOuts <- createResultsFrame(practiceRuns)
-outs <- createResultsFrame(simRuns)
-
-currentYear <- 1
-
-practiceYear <- 1
 
 
 ##
@@ -98,12 +61,12 @@ tagList(
                           });
                           ')),
 
-navbarPage("Ranch Drought", id = "navBar",
+fluidPage("Ranch Drought", id = "navBar",
 
- uiOutput("test"),
- # for(i in 1:3){
- #   uiOutput("test")
- # }
+
+tabsetPanel(
+  
+  
  tabPanel("Instructions",
     fluidRow(
       column(12,
@@ -137,14 +100,22 @@ navbarPage("Ranch Drought", id = "navBar",
         )
         
     )),
-  
-  tabPanel("Year 1",
+
+ tabPanel("Year 1",
     fluidRow(
-      h4("Year 1 Winter Finance Assessment"),
-      p(paste("Your Current Networth is:"))
+      column(6,
+        uiOutput("winterInfo"),
+        fluidRow(column(12, style = "background-color:white;", div(style = "height:50px;"))),
+        uiOutput("decision1")
+      ),
+      column(2,
+        fluidRow(column(12, style = "background-color:white;", div(style = "height:170px;"))),
+        actionButton("year1Start", "Begin Simulation")
+      )
     )
-    ),
-  
+ ),
+ 
+
   
   tabPanel("Results",
     fluidPage(
@@ -188,5 +159,4 @@ navbarPage("Ranch Drought", id = "navBar",
   )
 )
 )
-
-
+)
