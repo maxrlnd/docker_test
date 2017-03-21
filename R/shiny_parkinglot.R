e00c90e736069ba039b867281a6c6e557315cd0d
Adam McCurdy <admccurdy@gmail.com>
2017-03-21 06:22:33 -0600

    Removed old code

--- R/shiny_parkinglot.R
+++ R/shiny_parkinglot.R
@@ -144,3 +144,12 @@ tabPanel("Input",
            )
          )
 ),
+
+output$map <- renderLeaflet({
+  leaflet() %>%
+    addTiles(
+      urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+      attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+    ) %>%
+    setView(lng = -93.85, lat = 37.45, zoom = 4)
+})
