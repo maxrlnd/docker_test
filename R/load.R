03f66b677fa41282d668fc4cdbab4045ef41ad3a
Adam McCurdy <admccurdy@gmail.com>
2017-03-30 12:50:00 -0600

    removed NOAA dependancy

--- R/load.R
+++ R/load.R
@@ -32,11 +32,12 @@ load("data/insurance_base.RData") # Load base insurance data
 # Load gridded precip
 # Get the gridded precip file if not in 'data' folder
 # ideally I can get this to work with git-lfs in the future...
-if(!"noaaIndex.RData" %in% list.files("data")){
-  cat("No NOAA Index data found. Fetching the files...\n")
-  download.file("https://o365coloradoedu-my.sharepoint.com/personal/jotu9073_colorado_edu/_layouts/15/guestaccess.aspx?guestaccesstoken=CLfTQ%2fVIj3v7cel5UkM65Noqx%2bHEgIDoS0lzqM5FHUw%3d&docid=08a40bf084add47629568d4d4161e283e&rev=1",destfile="data/noaaIndex.RData")
-}
-load("data/noaaIndex.RData")
+
+# if(!"noaaIndex.RData" %in% list.files("data")){
+#   cat("No NOAA Index data found. Fetching the files...\n")
+#   download.file("https://o365coloradoedu-my.sharepoint.com/personal/jotu9073_colorado_edu/_layouts/15/guestaccess.aspx?guestaccesstoken=CLfTQ%2fVIj3v7cel5UkM65Noqx%2bHEgIDoS0lzqM5FHUw%3d&docid=08a40bf084add47629568d4d4161e283e&rev=1",destfile="data/noaaIndex.RData")
+# }
+# load("data/noaaIndex.RData")
 
 ## Miscellaneous supporting var's/data
 # State code - for pulling COOP sites & mlra forage weights
