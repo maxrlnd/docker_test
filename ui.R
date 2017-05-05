86fe9c7c5b4c0b983af7614ca5917197ff58389c
tshrum <trisha.shrum@colorado.edu>
2017-05-05 12:03:30 -0600

    Commented out the instructions, background info and release agreement tabs. Moved "simStart" button to debug page

--- ui.R
+++ ui.R
@@ -84,7 +84,9 @@ tabsetPanel(id = "mainPanels",
             actionButton("runCode", "Run Code"),
             textInput("insChange", "Enter True or False to use insurance or not"),
             actionButton("applyInsChange", "Change Insurance"),
-            actionButton("reset_button", "Reset Page")
+            actionButton("reset_button", "Reset Page"),
+            br(),
+            actionButton("simStart", "Begin Ranch Game")
             # actionButton("saveInputs", "Save all Input")
           )),
  # tabPanel("Input",
@@ -168,131 +170,131 @@ tabsetPanel(id = "mainPanels",
  #          )
  # ),
  
- tabPanel("Instructions",
-    fluidRow(
-      column(12,
-             h4("Instructions"),
-             p("Thank you for participating in our study. In this study, you 
-                will be participating in a game where you act as the owner of a 
-                cow-calf ranch. Please read these instructions carefully before 
-                you proceed to the next screen."),
-             p("First, you will read information you’ll need to “run” a ranch in 
-               this game. Read this information carefully, but don’t worry about 
-               memorizing everything. This information will be available to you 
-               throughout the simulation by clicking back on the information 
-               tabs."),
-             p("Next, you will take a comprehension quiz. You can look back at 
-                the information and instructions while you take the quiz, but 
-                you must answer all questions correctly to continue the game. If 
-                you do not answer them correctly the first time, you will have 
-                two more chances to answer the questions you missed."),
-             p("After you pass the comprehension quiz, you will do five practice 
-               rounds of the ranching game. What you do during these practice 
-               rounds will have NO impact on your game that counts. Feel free to 
-               play around with your decisions to better understand how they 
-               affect your outcomes."),
-             p("Next, you will play ten rounds of the ranching game. The 
-               rainfall will be different in each year and is drawn from a 
-               historical distribution of rainfall in Colorado. The rainfall 
-               will be different than in the practice rounds. Make the best 
-               choices you can about buying hay and selling cows and calves. 
-               These choices will affect your net worth at the end of the game."),
-             p("At the end of the game, your net worth (your bank account plus 
-              the value of the herd) will be translated into a real-world MTurk 
-              bonus at a rate of $100,000 of simulation money to $1 of real 
-              world money. You will receive your bonus within one month of 
-              completing this study."),
-             p("Next you will answer a series of basic demographic questions 
-               that we will only use for control variables, you will not be 
-               identified based on your answers."),
-             p(" Finally, we will ask you to choose from a series of ten 
-              lotteries. One person in this study will be chosen and one 
-              randomly selected choice will be implemented. The winner will 
-              receive a bonus according to the lottery that is chosen."),
-             p("You must complete the full study to be eligible for the bonuses. 
-              You may skip demographic questions that you do not feel 
-              comfortable answering, but you must continue to the end of the 
-              survey and complete the final round of questions."),
-             br(),
-             
-             h4("Release/Agreeement"),
-             p("You must agree to continue"),
-             actionButton("agree", "I Agree")
-      )
-    )),
- 
-
- tabPanel("Background Info",
-          fluidRow(
-            column(12,
-              h4("Ranching Information"),
-              p("You run a ranch which breeds and raises calves to be sold at 
-                auction."),
-              p("Calves are born in early spring and are raised by drinking milk 
-                from their mother until they reach a weight of about 600 pounds. 
-                Once the calves stop taking milk from their mothers they are 
-                called weaned calves. Because the calves gain weight throughout 
-                the summer from their mothers’ milk, it is important that the 
-                mother cows are well fed and healthy."),
-              p("Each year, most weaned calves and some mother cows are sold at 
-                auction in October. If a weaned calf is not sold it will stay on 
-                the ranch for two years until it reaches maturity and can be 
-                bred. If a mother cow is sold at auction it is called culling."),
-              p("There is a delicate balance between the size of a ranch and the 
-                number of mother cows and calves that can be kept there. If too 
-                many cattle are grazing on the ranch, they will eat the grass 
-                down to the dirt and it won’t grow back as well for the next 
-                season. This means there is less food for next year’s herd. If 
-                too few cattle are present on the land the ranch cannot be 
-                profitable and stay in business."),
-              p("The ranch operates by allowing the mother cows to graze on 
-                pasture land. In years with less precipitation the ranch must 
-                purchase extra hay because there is less grass available and 
-                calves will not be able to reach their target weight. Skinny 
-                calves mean less revenue when they go to market. Feeding the 
-                mother cows more hay keeps the mother cows healthy and able to 
-                produce milk for the calves. Also, if the mother cows aren’t 
-                healthy, they won’t produce as many cows for the next season."),
-              p("(Insurance treatment only)"),
-              p("The rainfall each year is unpredictable, but it can have a big 
-                impact on ranchers’ bottom lines. To help protect their income, 
-                ranchers can purchase insurance that will send them a check when 
-                droughts occur during months when they normally grow a lot of 
-                grass. The worse the drought, the bigger the check. The 
-                insurance payment is based off the total rainfall per month as 
-                compared to historical normal: if the rainfall is below the 
-                historical norms then you are entitled to an insurance payout."),
-              br(),
-              
-              h4("Facts About Your Ranch"),
-              tags$li("Your ranch is located about an hour northeast of Denver, 
-                Colorado and includes 3000 acres of private land."),
-              tags$li("Your herd has 600 cows and 528 calves."),
-              tags$li("On your ranch, calves are born between February and 
-                March, and are sold in October."),
-              tags$li("Under normal conditions, your ranch has a calf birth and 
-                weaning success rate of 88%."),
-              tags$li("Your ranch usually sells 75% of calves and 15% of mother 
-                cows each year."),
-              tags$li("The expected weight for a calf when it is ready to sell 
-                is 600lb, while a mother cow will be about 1,400lb when sold."),
-              tags$li("The market price for calves is $1.45/lb. Mother cows are 
-                sold for $850 per cow."),
-              tags$li("Your ranch can support 600 cow-calf pairs on a total of 
-                5000 acres without damaging the grass in a normal year."),
-              tags$li("Each mother cow/calf have a normal yearly operating cost 
-                of $500, and your ranch has an extra yearly base operating cost 
-                of $20,000."),
-              tags$li("(Insurance only) Each year, you buy $X of insurance for 
-                your ranch. The biggest payout you could get is $Y."),
-              br(),
-              
-              h5("In the next section, you will take a quiz to ensure that you
-                 know the basic information you will need in this game."),
-              actionButton("simStart", "Begin Ranch Game")
-              
-            )
-          )),
+ # tabPanel("Instructions",
+ #    fluidRow(
+ #      column(12,
+ #             h4("Instructions"),
+ #             p("Thank you for participating in our study. In this study, you 
+ #                will be participating in a game where you act as the owner of a 
+ #                cow-calf ranch. Please read these instructions carefully before 
+ #                you proceed to the next screen."),
+ #             p("First, you will read information you’ll need to “run” a ranch in 
+ #               this game. Read this information carefully, but don’t worry about 
+ #               memorizing everything. This information will be available to you 
+ #               throughout the simulation by clicking back on the information 
+ #               tabs."),
+ #             p("Next, you will take a comprehension quiz. You can look back at 
+ #                the information and instructions while you take the quiz, but 
+ #                you must answer all questions correctly to continue the game. If 
+ #                you do not answer them correctly the first time, you will have 
+ #                two more chances to answer the questions you missed."),
+ #             p("After you pass the comprehension quiz, you will do five practice 
+ #               rounds of the ranching game. What you do during these practice 
+ #               rounds will have NO impact on your game that counts. Feel free to 
+ #               play around with your decisions to better understand how they 
+ #               affect your outcomes."),
+ #             p("Next, you will play ten rounds of the ranching game. The 
+ #               rainfall will be different in each year and is drawn from a 
+ #               historical distribution of rainfall in Colorado. The rainfall 
+ #               will be different than in the practice rounds. Make the best 
+ #               choices you can about buying hay and selling cows and calves. 
+ #               These choices will affect your net worth at the end of the game."),
+ #             p("At the end of the game, your net worth (your bank account plus 
+ #              the value of the herd) will be translated into a real-world MTurk 
+ #              bonus at a rate of $100,000 of simulation money to $1 of real 
+ #              world money. You will receive your bonus within one month of 
+ #              completing this study."),
+ #             p("Next you will answer a series of basic demographic questions 
+ #               that we will only use for control variables, you will not be 
+ #               identified based on your answers."),
+ #             p(" Finally, we will ask you to choose from a series of ten 
+ #              lotteries. One person in this study will be chosen and one 
+ #              randomly selected choice will be implemented. The winner will 
+ #              receive a bonus according to the lottery that is chosen."),
+ #             p("You must complete the full study to be eligible for the bonuses. 
+ #              You may skip demographic questions that you do not feel 
+ #              comfortable answering, but you must continue to the end of the 
+ #              survey and complete the final round of questions."),
+ #             br(),
+ #             
+ #             h4("Release/Agreeement"),
+ #             p("You must agree to continue"),
+ #             actionButton("agree", "I Agree")
+ #      )
+ #    )),
+ # 
+ # 
+ # tabPanel("Background Info",
+ #          fluidRow(
+ #            column(12,
+ #              h4("Ranching Information"),
+ #              p("You run a ranch which breeds and raises calves to be sold at 
+ #                auction."),
+ #              p("Calves are born in early spring and are raised by drinking milk 
+ #                from their mother until they reach a weight of about 600 pounds. 
+ #                Once the calves stop taking milk from their mothers they are 
+ #                called weaned calves. Because the calves gain weight throughout 
+ #                the summer from their mothers’ milk, it is important that the 
+ #                mother cows are well fed and healthy."),
+ #              p("Each year, most weaned calves and some mother cows are sold at 
+ #                auction in October. If a weaned calf is not sold it will stay on 
+ #                the ranch for two years until it reaches maturity and can be 
+ #                bred. If a mother cow is sold at auction it is called culling."),
+ #              p("There is a delicate balance between the size of a ranch and the 
+ #                number of mother cows and calves that can be kept there. If too 
+ #                many cattle are grazing on the ranch, they will eat the grass 
+ #                down to the dirt and it won’t grow back as well for the next 
+ #                season. This means there is less food for next year’s herd. If 
+ #                too few cattle are present on the land the ranch cannot be 
+ #                profitable and stay in business."),
+ #              p("The ranch operates by allowing the mother cows to graze on 
+ #                pasture land. In years with less precipitation the ranch must 
+ #                purchase extra hay because there is less grass available and 
+ #                calves will not be able to reach their target weight. Skinny 
+ #                calves mean less revenue when they go to market. Feeding the 
+ #                mother cows more hay keeps the mother cows healthy and able to 
+ #                produce milk for the calves. Also, if the mother cows aren’t 
+ #                healthy, they won’t produce as many cows for the next season."),
+ #              p("(Insurance treatment only)"),
+ #              p("The rainfall each year is unpredictable, but it can have a big 
+ #                impact on ranchers’ bottom lines. To help protect their income, 
+ #                ranchers can purchase insurance that will send them a check when 
+ #                droughts occur during months when they normally grow a lot of 
+ #                grass. The worse the drought, the bigger the check. The 
+ #                insurance payment is based off the total rainfall per month as 
+ #                compared to historical normal: if the rainfall is below the 
+ #                historical norms then you are entitled to an insurance payout."),
+ #              br(),
+ #              
+ #              h4("Facts About Your Ranch"),
+ #              tags$li("Your ranch is located about an hour northeast of Denver, 
+ #                Colorado and includes 3000 acres of private land."),
+ #              tags$li("Your herd has 600 cows and 528 calves."),
+ #              tags$li("On your ranch, calves are born between February and 
+ #                March, and are sold in October."),
+ #              tags$li("Under normal conditions, your ranch has a calf birth and 
+ #                weaning success rate of 88%."),
+ #              tags$li("Your ranch usually sells 75% of calves and 15% of mother 
+ #                cows each year."),
+ #              tags$li("The expected weight for a calf when it is ready to sell 
+ #                is 600lb, while a mother cow will be about 1,400lb when sold."),
+ #              tags$li("The market price for calves is $1.45/lb. Mother cows are 
+ #                sold for $850 per cow."),
+ #              tags$li("Your ranch can support 600 cow-calf pairs on a total of 
+ #                5000 acres without damaging the grass in a normal year."),
+ #              tags$li("Each mother cow/calf have a normal yearly operating cost 
+ #                of $500, and your ranch has an extra yearly base operating cost 
+ #                of $20,000."),
+ #              tags$li("(Insurance only) Each year, you buy $X of insurance for 
+ #                your ranch. The biggest payout you could get is $Y."),
+ #              br(),
+ #              
+ #              h5("In the next section, you will take a quiz to ensure that you
+ #                 know the basic information you will need in this game."),
+ #              actionButton("simStart", "Begin Ranch Game")
+ #              
+ #            )
+ #          )),
   
  #  ## Panel for comprehension quiz
  # tabPanel("Quiz",
