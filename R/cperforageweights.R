b3e16d2e947ade121dbe1ee304c2d8b31ec13528
Adam McCurdy <admccurdy@gmail.com>
2017-05-05 06:51:43 -0600

    fixed forage weights

--- R/cperforageweights.R
+++ R/cperforageweights.R
@@ -1,6 +1,4 @@
-cpermlra <- c(0.01, 0.01, 0.05, 0.08,0.17,0.21,0.17,0.13,0.11,0.05,0.01,0.01)
-months <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+cpermlra <- c(0.0, 0.0, 0.02, 0.08,0.20,0.28,0.15,0.12,0.10,0.05,0.0,0.0) #changed sep from .11 to .10months <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
 cper <- matrix(cpermlra, nrow = 1, ncol = 12)
 colnames(cper) <- months
-
 
