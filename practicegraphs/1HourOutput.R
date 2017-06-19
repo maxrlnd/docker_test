fc20866f1cdefda5c08cb391c707017b770a7ddd
EvanLih <evli6488@colorado.edu>
2017-06-18 21:16:09 -0600

    updated coding

--- practicegraphs/1HourOutput.R
+++ practicegraphs/1HourOutput.R
@@ -9,7 +9,7 @@ experimentallist <- data.frame(mTurkID=numeric(200), herdsize=numeric(200), year
 
 experimentallist$mTurkID = sample(30:60, 200, replace = TRUE)
 
-experimentallist$herdsize = sample(400:600, 200, replace = FALSE)
+experimentallist$herdsize = rnorm(200, mean = 500, sd=1)
 
 experimentallist$year = sample(2000:2011, 200, replace = TRUE)
 
@@ -18,15 +18,25 @@ experimentallist$insurance = ifelse(experimentallist$mTurkID < 39, "noinsurance"
 experimentallist$mTurkID =  factor(experimentallist$mTurkID )
 
 
- experimentallist$name = ifelse(experimentallist$mTurkID == 30, "Jeff",
+ experimentallist$name = ifelse(experimentallist$mTurkID == 30, "Test",
 ifelse(experimentallist$mTurkID == 31, "Rob",
        ifelse(experimentallist$mTurkID == 32, "Simon",
               ifelse(experimentallist$mTurkID == 33,"Yousef",
               ifelse(experimentallist$mTurkID == 34, "Leah",                   ifelse(experimentallist$mTurkID == 35,"Job",ifelse(experimentallist$mTurkID == 36,"Lob",ifelse(experimentallist$mTurkID == 37,"Mob",ifelse(experimentallist$mTurkID == 38,"Sob",ifelse(experimentallist$mTurkID == 39,"Aob",ifelse(experimentallist$mTurkID == 40,"Rub",ifelse(experimentallist$mTurkID == 41,"Sub",ifelse(experimentallist$mTurkID == 42,"Dud",ifelse(experimentallist$mTurkID == 43,"Lub",ifelse(experimentallist$mTurkID == 44,"B12b",ifelse(experimentallist$mTurkID == 45,"Bsb",ifelse(experimentallist$mTurkID == 46,"B13b",ifelse(experimentallist$mTurkID == 47,"B2b",ifelse(experimentallist$mTurkID == 48,"B14b",ifelse(experimentallist$mTurkID == 49,"B6b",ifelse(experimentallist$mTurkID == 50,"B15b",ifelse(experimentallist$mTurkID == 51,"B4b",ifelse(experimentallist$mTurkID == 52,"B16b",ifelse(experimentallist$mTurkID == 53,"B8b",ifelse(experimentallist$mTurkID == 54,"B17b",ifelse(experimentallist$mTurkID == 55,"B9b",ifelse(experimentallist$mTurkID == 56,"B18b",ifelse(experimentallist$mTurkID == 57,"B10b",ifelse(experimentallist$mTurkID == 58,"B19b",ifelse(experimentallist$mTurkID == 59,"B11b",ifelse(experimentallist$mTurkID == 60,"B20b", NA)))))))))))))))))))))))))))))))
+ 
+ library(randomNames)
+ 
+ names = data.frame(randomNames(30))
+ 
 
-ggplot(experimentallist, aes(year, herdsize, group = insurance, color = insurance)) +
+
+
+library(ggplot2)
+ggplot(experimentallist, aes(year, herdsize, group = mTurkID, color =  mTurkID)) +
   geom_line() +
   ggtitle("Herd Size per Year, Insurance vs. No Insurance")
+
+names
 
 
 library("plyr")
